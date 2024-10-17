48a6782161403cbfee2a2a599c8050eba3ba0c3b
Akash Tiwari <154492266+shounak-tiwari@users.noreply.github.com>
2024-10-17 15:58:27 +0530

    Add files via upload

---
+++ Oct 17/First.py
@@ -0,0 +1,23 @@
+#basic syntax of python programming 
+"""
+class keywords name of class : 
+	
+	pass keywords (it show class is empty )
+
+object = class name 
+
+"""
+class Demo:
+	#behaviour of class
+    def welcome():
+        print("Wellcome in world of object orinted programming ")
+    
+
+object_name = Demo
+
+#call behaviour using class object
+
+object_name.welcome()
+
+
+
